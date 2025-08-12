e1229babd62164980a22c7c449e4c703e18e37dd
Pedro-j-code <pessoinhaaa2@gmail.com>
2025-08-12 14:52:34 -0300

    {solução concluída do {Buscando Um Número}}

---
+++ Buscando Um Número.py
@@ -0,0 +1,8 @@
+
+numeros = [987654321, 2, 7654321, 56, 1234567, 1, 88888, 3, 42, 999999, 5, 1000000000, 13, 101010, 7, 444, 9, 2, 13, 9]
+achar = int(input())
+if achar in numeros:
+    print("True")
+
+else:
+    print("False")
