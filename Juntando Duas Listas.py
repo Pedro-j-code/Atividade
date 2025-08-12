843abd71e33e022a7872f4878f688470c7eefc57
Pedro-j-code <pessoinhaaa2@gmail.com>
2025-08-12 14:47:46 -0300

    {solução concluída do {Juntando Duas Listas}}

---
+++ Juntando Duas Listas.py
@@ -0,0 +1,4 @@
+natureza = ["rio", "floresta", "leão", "montanha", "oceano", "estrela", "vento", "cachoeira", "lua", "girassol"]
+tecnologia = ["algoritmo", "API", "blockchain", "código", "software", "nuvem", "hardware", "interface", "pixel", "servidor"]
+lista3 = (natureza + tecnologia)
+print(lista3)
